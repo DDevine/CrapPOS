182ba71fb6611800a5bde9103a44bf50eeeb9652
Daniel Devine <devine@ddevnet.net>
2013-10-01 21:17:51 +1000

    Stubbing

--- src/au/edu/griffith/ict/Main.java
+++ src/au/edu/griffith/ict/Main.java
@@ -1,5 +1,34 @@
 package au.edu.griffith.ict;
+import au.edu.griffith.ict.*;
 
-public class Main{
+
+public class Main {
+	// custManager: CustomerManager
+	// itemManager: Menu
+	// orders: Order[*]
+	// users: User[*]
+
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}	
+
+	//void addOrder(Order order){
+		// Adds an order to the database/array
+	//}
 	
+	//void removeOrder(Order order){
+		// Removes an Order from the database/array
+	//}
+
+	//Order[]  getOrders(Customer customer){
+		// Gets an array of Order objects for a given Customer.
+	//}
+
+	//float getDayTotal(){
+		// Return the total takings for the day... 
+	//}
+	
+	//void display(){
+		// Display... what?
+	//}
 }
--- src/au/edu/griffith/ict/Menu.java
+++ src/au/edu/griffith/ict/Menu.java
@@ -4,5 +4,26 @@ public class Menu{
 	private MenuItem[] menuItems; // Use menuItem.getNo() as UNIQUE ID here.
 	
 	private int nextIDNo;
-	//Why the hell does our UML have a price field in the MENU for?
+	//Why the hell does our UML have a price field in the MENU for?\
+	
+
+	// MenuItem getItem(int itemNo){
+		// Get a MenuItem by associated item number.
+	// }
+
+	// void updateItem(MenuItem item){
+		// Update the given Menu item...
+	// }
+
+	// void newItem(MenuItem item){
+		// Adds an item to the menu.
+	// }
+	//
+	// int nextItemNo(){
+		// returns the next available item number.
+	// }
+	//
+	// boolean removeItem(int itemNo){
+		// Removes an item from the menu. Returns true on success.
+	// }
 }

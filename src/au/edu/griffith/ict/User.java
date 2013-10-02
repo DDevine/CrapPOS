0563ecdbf06d9afae5c0bcc82aae09c4f3c956d3
Daniel Devine <devine@ddevnet.net>
2013-10-02 12:45:41 +1000

    Adding JavaDoc

--- src/au/edu/griffith/ict/User.java
+++ src/au/edu/griffith/ict/User.java
@@ -1,8 +1,14 @@
 package au.edu.griffith.ict;
 
 public class User{
+
+    /** The user's password */
 	private String password;
+	
+	/** The user's staff number */
 	private int staffNo;
+	
+	/** The user's administrative status */
 	private boolean admin;
 	
 	public User(int staffNo, String pass, boolean admin){
@@ -11,12 +17,26 @@ public class User{
 		this.admin = admin;
 	}
 	
+	/**
+	* Get the user's staff number.
+	* @return An int which represents the stuff number.
+	*/
 	public int getStaffNo(){
 		return staffNo;
 	}
+	
+	/**
+	* Get the user's password
+	* @return A String containing the user's password.
+	*/
 	public String getPassword(){
 		return password;
 	}
+	
+	/**
+	* Get the user's Administrator status.
+	* @return If the user is an Administrator, returns True.
+	*/
 	public boolean isAdmin(){
 		return admin;
 	}

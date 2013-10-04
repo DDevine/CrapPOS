4dc0ae12aa545a56505986a76bcee0e87f2d59dc
Daniel Devine <devine@ddevnet.net>
2013-10-04 14:12:23 +1000

    Trying to get java.* out of relations... Will probably have to manually specifify relations.

--- docs/makedocs.sh
+++ docs/makedocs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # You need to install graphvis, javadoc and umlgraph
-javadoc -doclet org.umlgraph.doclet.UmlGraphDoc -docletpath /usr/share/java/umlgraph.jar -private -attributes -classpath -types -all ../src ../src/au/edu/griffith/ict/*.java
+javadoc -doclet org.umlgraph.doclet.UmlGraphDoc -docletpath /usr/share/java/umlgraph.jar -private -attributes -types -all -inferdep -inferdepinpackage -inferdepvis public -classpath ../src ../src/au/edu/griffith/ict/*.java

1e296febba516e009c0f850c60da57225460e874
Daniel Devine <devine@ddevnet.net>
2013-10-04 13:24:41 +1000

    Adding UML to JavaDoc

--- docs/makedocs.sh
+++ docs/makedocs.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-javadoc -classpath ../src ../src/au/edu/griffith/ict/*.java
+# You need to install graphvis, javadoc and umlgraph
+javadoc -doclet org.umlgraph.doclet.UmlGraphDoc -docletpath /usr/share/java/umlgraph.jar -private -attributes -classpath -types -all ../src ../src/au/edu/griffith/ict/*.java
